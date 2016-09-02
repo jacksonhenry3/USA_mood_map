b83ab1af3b92859b616f3985f86b5122ccf9499b
Jackson Henry <jacksonhenry3@gmail.com>
2016-09-02 13:30:37 -0700

    live updates working!

--- js/main.js
+++ js/main.js
@@ -1,30 +1,54 @@
+var runs =0;
+var colorFunction = d3.interpolate("rgb(200,0,0)", "rgb(0,200,80)");
+var sampleData
+function tooltipHtml(n, mood)
+{ /* function to create html content string in tooltip div. */
+	return "<h4> The average mood of a tweet about "+n+" is "+mood+"</h4>"
+}
 window.setInterval(
-	function(){
+	function()
+		{
 		var moodData 
-		$.getJSON("moodData/data.json", function(json) {
-		    moodData = json
-		    console.log(moodData['California'])		
+		$.getJSON("moodData/data.json", 
+			function(json) 
+				{
+				moodData = json;
+				statesName = {'AK': 'Alaska','AL': 'Alabama','AR': 'Arkansas','AS': 'American Samoa','AZ': 'Arizona','CA': 'California','CO': 'Colorado','CT': 'Connecticut','DC': 'District of Columbia','DE': 'Delaware','FL': 'Florida','GA': 'Georgia','GU': 'Guam','HI': 'Hawaii','IA': 'Iowa','ID': 'Idaho','IL': 'Illinois','IN': 'Indiana','KS': 'Kansas','KY': 'Kentucky','LA': 'Louisiana','MA': 'Massachusetts','MD': 'Maryland','ME': 'Maine','MI': 'Michigan','MN': 'Minnesota','MO': 'Missouri','MP': 'Northern Mariana Islands','MS': 'Mississippi','MT': 'Montana','NA': 'National','NC': 'North Carolina','ND': 'North Dakota','NE': 'Nebraska','NH': 'New Hampshire','NJ': 'New Jersey','NM': 'New Mexico','NV': 'Nevada','NY': 'New York','OH': 'Ohio','OK': 'Oklahoma','OR': 'Oregon','PA': 'Pennsylvania','PR': 'Puerto Rico','RI': 'Rhode Island','SC': 'South Carolina','SD': 'South Dakota','TN': 'Tennessee','TX': 'Texas','UT': 'Utah','VA': 'Virginia','VI': 'Virgin Islands','VT': 'Vermont','WA': 'Washington','WI': 'Wisconsin','WV': 'West Virginia','WY': 'Wyoming'}
 
-states = {'AK': 'Alaska','AL': 'Alabama','AR': 'Arkansas','AS': 'American Samoa','AZ': 'Arizona','CA': 'California','CO': 'Colorado','CT': 'Connecticut','DC': 'District of Columbia','DE': 'Delaware','FL': 'Florida','GA': 'Georgia','GU': 'Guam','HI': 'Hawaii','IA': 'Iowa','ID': 'Idaho','IL': 'Illinois','IN': 'Indiana','KS': 'Kansas','KY': 'Kentucky','LA': 'Louisiana','MA': 'Massachusetts','MD': 'Maryland','ME': 'Maine','MI': 'Michigan','MN': 'Minnesota','MO': 'Missouri','MP': 'Northern Mariana Islands','MS': 'Mississippi','MT': 'Montana','NA': 'National','NC': 'North Carolina','ND': 'North Dakota','NE': 'Nebraska','NH': 'New Hampshire','NJ': 'New Jersey','NM': 'New Mexico','NV': 'Nevada','NY': 'New York','OH': 'Ohio','OK': 'Oklahoma','OR': 'Oregon','PA': 'Pennsylvania','PR': 'Puerto Rico','RI': 'Rhode Island','SC': 'South Carolina','SD': 'South Dakota','TN': 'Tennessee','TX': 'Texas','UT': 'Utah','VA': 'Virginia','VI': 'Virgin Islands','VT': 'Vermont','WA': 'Washington','WI': 'Wisconsin','WV': 'West Virginia','WY': 'Wyoming'}
+				
 
-		function tooltipHtml(n, mood){ /* function to create html content string in tooltip div. */
-		    return "<h4> The average mood of a tweet about "+n+" is "+mood+"</h4>"
-		  }
-		  
-		  var sampleData ={}; /* Sample random data. */ 
-		  ["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
-		  "ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH", 
-		  "MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT", 
-		  "CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN", 
-		  "WI", "MO", "AR", "OK", "KS", "LA", "VA"]
-		    .forEach(function(d){ 
-		      var mood = moodData[states[d]]
-		      sampleData[d]={mood:mood,color:d3.interpolate("rgb(200,0,0)", "rgb(0,200,80)")((mood+1)/2)}; 
-		    });
+				sampleData ={}; /* Sample random data. */ 
+				["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
+				"ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH", 
+				"MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT", 
+				"CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN", 
+				"WI", "MO", "AR", "OK", "KS", "LA", "VA"]
+				.forEach
+				(
+					function(d)
+					{ 
+						var mood      = moodData[statesName[d]]
+						sampleData[d] = 
+										{
+											mood:mood,
+											color:colorFunction((mood+1)/2)
+										}; 
+					}
+				);
 
-		  
-		  /* draw states on id #statesvg */ 
-		  uStates.draw("#statesvg", sampleData, tooltipHtml);
-		  
-		  d3.select(self.frameElement).style("height", "60px"); 
-});},200)
+				if (runs==0)
+				{
+					
+					states.draw(sampleData, tooltipHtml);
+				}
+				if (runs>1)
+				{
+					states.update(sampleData, tooltipHtml);
+				};
+				runs = runs+1
+				
+				d3.select(self.frameElement).style("height", "60px"); 
+				}
+			);
+		}
+,200)
