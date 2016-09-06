74743834e488dab7e210ddea91359c3651297ce9
Jackson Henry <jacksonhenry3@gmail.com>
2016-09-06 08:13:40 -0700

    get tweets by geo tag instead of words

--- backend/generateStateMoodData.py
+++ backend/generateStateMoodData.py
@@ -1,76 +1,78 @@
+from json import dump
+from re import sub
 import tweepy
-import re
-from sklearn.externals import joblib
-from numpy import loadtxt,transpose
-import time
-
-start_time = time.time()
-
-def oAuth():
-    print("authorising")
-    # Twitter Oauth handshake
-    OAuthkeys = loadtxt("config",dtype = str)
-    OAuthkeys = dict(zip(OAuthkeys[:,0], OAuthkeys[:,1]))
-    auth      = tweepy.OAuthHandler(OAuthkeys["consumerKey"], OAuthkeys["consumerSecret"])
-    auth.set_access_token(OAuthkeys["accessToken"], OAuthkeys["accessTokenSecret"])
-    api       = tweepy.API(auth)
-    return(api)
-
-# generate the model
-print('generating model')
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
-import pandas as pd
+from pandas import read_csv
 
+
+def oAuth():
+    print("authorising")
+    # Twitter Oauth handshake
+    OAuthkeys = read_csv("config", skiprows=None).values
+    OAuthkeys = {arr[0]: arr[1] for arr in OAuthkeys}
+    auth = tweepy.OAuthHandler(OAuthkeys["consumerKey"],
+                               OAuthkeys["consumerSecret"])
+    auth.set_access_token(OAuthkeys["accessToken"],
+                          OAuthkeys["accessTokenSecret"])
+    api = tweepy.API(auth)
+    return(api)
+
+# generate the model
+print ("loading data")
 
 # 0 is sad, 1 is happy
-#dat    = pd.read_csv('sentimentTrainingData/cleanedTweetSentimentAnalysisDataset.txt',sep=None,engine='python',usecols = [1,3]).values
-dat    = pd.read_csv('sentimentTrainingData/xab',sep=None,engine='python',usecols = [1,3]).values
-tweets = dat[:,1]
-happy  = dat[:,0].astype(float)
+# dat    = pd.read_csv('sentimentTrainingData/cleanedTweetSentimentAnalysisDataset.txt',sep=None,engine='python',usecols = [1,3]).values
+dat = read_csv('sentimentTrainingData/cleanedTweetSentimentAnalysisDataset.txt'
+               , sep=None, engine='python', usecols=[1, 3]).values
+tweets = dat[:, 1]
+happy = dat[:, 0].astype(float)
 
-count_vect        = CountVectorizer()
-X_train_counts    = count_vect.fit_transform(tweets)
+print('generating model')
+
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(tweets)
 tfidf_transformer = TfidfTransformer()
-X_train_tfidf     = tfidf_transformer.fit_transform(X_train_counts)
-clf               = MultinomialNB().fit(X_train_tfidf, happy)
-
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+clf = MultinomialNB().fit(X_train_tfidf, happy)
 
 class stateTwitterMoodListener(tweepy.StreamListener):
+    """ Class which extends tweepy stream listener """
 
     def __init__(self, states):
         self.states= states
         tweepy.StreamListener.__init__(self)
 
-
-        # self.startListening()
-
     def startListening(self):
-        self.stream = tweepy.Stream(auth = api.auth, listener=self)
-        print(self.states)
-        self.stream.filter(languages = ["en"],track=self.states.tolist(),async = True)
+        self.stream = tweepy.Stream(auth=api.auth, listener=self)
+        self.stream.filter(languages=["en"],
+                           locations=[-124.8,25.01,-66.97,49.21],async = True)
 
     def on_status(self, status):
 
         # get and process the tweet
-        txt    = status.text
-        txt    = re.sub('(RT )', '', txt)
-        txt    = re.sub("(@[^\s]+)", '', txt)
-        newDoc = [txt]
+        txt = status.text
+        txt = sub('(RT )', '', txt)
+        txt = sub("(@[^\s]+)", '', txt)
+        txt = [txt]
 
-        for state in self.states:
-            if state in (status.text or status.expanded_url or status.screen_name):
+        for i in range(len(self.states)):
+            if (self.states[i][0] or self.states[i][1]) in (status.place.full_name):
+                state = self.states[i][1]
                 # vectorize and extract tfidf
-                X_new_counts = count_vect.transform(newDoc)
+                X_new_counts = count_vect.transform(txt)
                 X_new_tfidf  = tfidf_transformer.transform(X_new_counts)
 
                 # predict tweet sentiment
                 predicted  = clf.predict(X_new_tfidf)
-                statesAndMoods[state][0] += (predicted[0]-.5)*2
-                statesAndMoods[state][1] += 1
+                stateTweets[state]["tweets"].append({"text":txt[0],"mood":(predicted[0]-.5)*2.})
+                if stateTweets[state]["totalTweets"]>=100:
+                    stateTweets[state]["tweets"].pop(0)
+                    print 'to many tweets about '+state
                 updateData()
+                break
 
     def apoptos(self):
         self.stream.disconnect()
@@ -78,45 +80,39 @@ class stateTwitterMoodListener(tweepy.StreamListener):
 
 
 def updateData():
-    f = open('../moodData/data.json','w')
-    dt = (time.time() - start_time)
-    f.write('{')
-    for state in statesAndMoods:
 
-        totalMood   = statesAndMoods[state][0]
-        totalTweets = statesAndMoods[state][1]
-        mood = str(float(totalMood)/totalTweets)
-        x = statesAndMoods[state][2]
-        y = statesAndMoods[state][3]
-        # if state != statesAndMoods.keys()[-1]:
-        #     f.write('{"'+state+'":['+mood+','+str(x)+','+str(y)+']},\n')
-        # else:
-        #     f.write('{"'+state+'":"'+str(float(totalMood)/totalTweets)+'"}\n')
-        #     f.write(']')
-        if state != statesAndMoods.keys()[-1]:
-            f.write('"'+state+'":'+mood+',\n')
-        else:
-            f.write('"'+state+'":'+mood+'}\n')
-            # f.write('}')
-        
+    # calculate meta data about all the states tweets
+    for state in stateTweets:
+        stateTweets[state]["totalTweets"] = len(stateTweets[state]["tweets"])
+        stateTweets[state]["totalMood"] = sum([tweet["mood"] for tweet in stateTweets[state]["tweets"]])
+        stateTweets[state]["averageMood"] = stateTweets[state]["totalMood"] / max(1,stateTweets[state]["totalTweets"])
 
-    
-    f.close()
+    with open("../moodData/data.json", "w") as statTwitterDataFile:
+        dump(stateTweets, statTwitterDataFile, indent=4, sort_keys=True) #dumps data as json
+
 
 api = oAuth()
 
-print("loading state data")
-statesAndCenters   = loadtxt("stateData/stateCenters.txt",dtype = str,delimiter  = ":",usecols  = (0,2))
-states             = statesAndCenters[:,0]
-centers = []
-for i,center in enumerate(statesAndCenters[:,1]):
-    coords = center.split(' ')
-    coords[0] = float(coords[0])
-    coords[1] = float(coords[1])
-    centers.append(coords)
-import numpy as np
-centers = np.array(centers)
-statesAndMoods = dict(zip(states, transpose([[0] * len(states),[1] * len(states),centers[:,0],centers[:,1]] ) ))
+states = [["HI", "Hawaii"], ["AK", "Alaska"], ["FL", "Florida"],
+          ["NH", "New Hampshire"], ["MI", "Michigan"], ["VT", "Vermont"],
+          ["ME", "Maine"], ["RI", "Rhode Island"], ["NY", "New York"],
+          ["PA", "Pennsylvania"], ["NJ", "New Jersey"], ["DE", "Delaware"],
+          ["MD", "Maryland"], ["VA", "Virginia"], ["WV", "West Virginia"],
+          ["OH", "Ohio"], ["IN", "Indiana"], ["IL", "Illinois"],
+          ["CT", "Connecticut"], ["WI", "Wisconsin"], ["NC", "North Carolina"],
+          ["DC", "Washington"], ["MA", "Massachusetts"], ["TN", "Tennessee"],
+          ["AR", "Arkansas"], ["MO", "Missouri"], ["GA", "Georgia"],
+          ["SC", "South Carolina"], ["KY", "Kentucky"], ["AL", "Alabama"],
+          ["LA", "Louisiana"], ["MS", "Mississippi"], ["IA", "Iowa"],
+          ["MN", "Minnesota"], ["OK", "Oklahoma"], ["TX", "Texas"],
+          ["NM", "New Mexico"], ["KS", "Kansas"], ["NE", "Nebraska"],
+          ["SD", "South Dakota"], ["ND", "North Dakota"], ["WY", "Wyoming"],
+          ["MT", "Montana"], ["CO", "Colarado"], ["ID", "Idaho"],
+          ["UT", "Utah"], ["AZ", "Arizona"], ["NV", "Nevada"], ["OR", "Oregon"],
+          ["WA", "Washington"], ["CA", "California"]]
+
+stateTweets = {state[1]: {'tweets': [], 'totalTweets': .01, 'totalMood': 0,
+               'averageMood': 0} for state in states}
 
 print("initializing listener")
 listener = stateTwitterMoodListener(states)
@@ -125,4 +121,4 @@ listener.startListening()
 print("initialized")
 testVar = raw_input("t for terminate")
 if testVar == 't':
-     listener.apoptos()
+    listener.apoptos()
--- js/main.js
+++ js/main.js
@@ -1,97 +1,21 @@
-var runs =0;
-var colorFunction = d3.scaleLinear()
-    .domain([-1, 0, 1])
-    .range(['#FF0000', '#EEEEEE', '#00FF00']);
+var svg       = $("#statesvg"),
+colorFunction = d3.scaleLinear().domain([-1, 0, 1]).range(['#FF0000', '#DDDDDD', '#00FF00']);
 
-
-var sampleData
-function tooltipHtml(n, mood)
+function tooltipHtml(n, mood,tt)
 { /* function to create html content string in tooltip div. */
-	return "<h4> The average mood of a tweet about "+n+" is "+mood+"</h4>"
+	return "<h4> The average mood of a tweet about "+n+" is "+mood+"</h4> <br> <h4> Calcuated from "+tt+" tweeets."
 }
-// window.setInterval(
-// 	function()
-// 		{
-		var moodData 
-		$.getJSON("moodData/data.json", 
-			function(json) 
-				{
-				moodData = json;
-				statesName = {'AK': 'Alaska','AL': 'Alabama','AR': 'Arkansas','AS': 'American Samoa','AZ': 'Arizona','CA': 'California','CO': 'Colorado','CT': 'Connecticut','DC': 'District of Columbia','DE': 'Delaware','FL': 'Florida','GA': 'Georgia','GU': 'Guam','HI': 'Hawaii','IA': 'Iowa','ID': 'Idaho','IL': 'Illinois','IN': 'Indiana','KS': 'Kansas','KY': 'Kentucky','LA': 'Louisiana','MA': 'Massachusetts','MD': 'Maryland','ME': 'Maine','MI': 'Michigan','MN': 'Minnesota','MO': 'Missouri','MP': 'Northern Mariana Islands','MS': 'Mississippi','MT': 'Montana','NA': 'National','NC': 'North Carolina','ND': 'North Dakota','NE': 'Nebraska','NH': 'New Hampshire','NJ': 'New Jersey','NM': 'New Mexico','NV': 'Nevada','NY': 'New York','OH': 'Ohio','OK': 'Oklahoma','OR': 'Oregon','PA': 'Pennsylvania','PR': 'Puerto Rico','RI': 'Rhode Island','SC': 'South Carolina','SD': 'South Dakota','TN': 'Tennessee','TX': 'Texas','UT': 'Utah','VA': 'Virginia','VI': 'Virgin Islands','VT': 'Vermont','WA': 'Washington','WI': 'Wisconsin','WV': 'West Virginia','WY': 'Wyoming'}
 
-				
+rescaleAndRedraw = function()
+{
+	p = .9
+	svg.empty();
+	svg.width(svg.parent().width()*p)
+	svg.height(svg.parent().width()*590/930*p)
+	$.getJSON("moodData/data.json", function(moodData){states.draw(moodData, tooltipHtml);});
+}
 
-				sampleData ={}; /* Sample random data. */ 
-				["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
-				"ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH", 
-				"MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT", 
-				"CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN", 
-				"WI", "MO", "AR", "OK", "KS", "LA", "VA"]
-				.forEach
-				(
-					function(d)
-					{ 
-						var mood      = moodData[statesName[d]]
-						sampleData[d] = 
-										{
-											mood:mood,
-											color:colorFunction(mood)
-										}; 
-					}
-				);
+window.onresize = rescaleAndRedraw
 
-				if (runs==0)
-				{
-					
-					states.draw(sampleData, tooltipHtml);
-				}
-				if (runs>1)
-				{
-					states.update(sampleData, tooltipHtml);
-				};
-				runs = runs+1
-				
-				d3.select(self.frameElement).style("height", "60px"); 
-				}
-			);
-// 		}
-// ,10000)
-
-
-
-window.onresize = function(event) {
-    $("#statesvg").empty();
-	var moodData 
-	$.getJSON("moodData/data.json" , Draw);
-};
-
-Draw = 		function(json) 
-			{
-			a=2
-			moodData = json;
-			statesName = {'AK': 'Alaska','AL': 'Alabama','AR': 'Arkansas','AS': 'American Samoa','AZ': 'Arizona','CA': 'California','CO': 'Colorado','CT': 'Connecticut','DC': 'District of Columbia','DE': 'Delaware','FL': 'Florida','GA': 'Georgia','GU': 'Guam','HI': 'Hawaii','IA': 'Iowa','ID': 'Idaho','IL': 'Illinois','IN': 'Indiana','KS': 'Kansas','KY': 'Kentucky','LA': 'Louisiana','MA': 'Massachusetts','MD': 'Maryland','ME': 'Maine','MI': 'Michigan','MN': 'Minnesota','MO': 'Missouri','MP': 'Northern Mariana Islands','MS': 'Mississippi','MT': 'Montana','NA': 'National','NC': 'North Carolina','ND': 'North Dakota','NE': 'Nebraska','NH': 'New Hampshire','NJ': 'New Jersey','NM': 'New Mexico','NV': 'Nevada','NY': 'New York','OH': 'Ohio','OK': 'Oklahoma','OR': 'Oregon','PA': 'Pennsylvania','PR': 'Puerto Rico','RI': 'Rhode Island','SC': 'South Carolina','SD': 'South Dakota','TN': 'Tennessee','TX': 'Texas','UT': 'Utah','VA': 'Virginia','VI': 'Virgin Islands','VT': 'Vermont','WA': 'Washington','WI': 'Wisconsin','WV': 'West Virginia','WY': 'Wyoming'}
-
-			
-
-			sampleData ={}; /* Sample random data. */ 
-			["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
-			"ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH", 
-			"MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT", 
-			"CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN", 
-			"WI", "MO", "AR", "OK", "KS", "LA", "VA"]
-			.forEach
-			(
-				DDDLDLPD
-			);
-			states.draw(sampleData, tooltipHtml);
-			};
-
-DDDLDLPD = function(d)
-				{ 
-					var mood      = moodData[statesName[d]];
-					sampleData[d] = 
-									{
-										mood : mood,
-										color : colorFunction(mood)
-									}; 
-				};
+rescaleAndRedraw()
+window.setInterval(function(){$.getJSON("moodData/data.json", function(moodData){states.update(moodData, tooltipHtml);});},1000)
